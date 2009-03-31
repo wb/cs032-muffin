9debaacf9c238066b1c214252258ecea6c46bf8b
jonathan.keisuke.yanagawa.kuo <jonathan.keisuke.yanagawa.kuo@100a9738-036a-11de-9992-cbdf800730d7>
2009-03-31 22:59:42 +0000

    game components

---
+++ 3D Renderer/3D Renderer/Menu.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace _3D_Renderer
+{
+    class Menu : DrawableGameComponent
+    {
+        Renderer _game;
+        GraphicsDevice graphics;
+        SpriteBatch spriteBatch;
+        Texture2D penguin;
+
+        public Menu(Renderer game)
+            : base(game)
+        {
+            _game = game;
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            spriteBatch = new SpriteBatch(_game.GraphicsDevice);
+            penguin = _game.Content.Load<Texture2D>("penguin");
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            graphics = _game.GraphicsDevice;
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Draw(GameTime gameTime)
+        {
+            // TODO: Add your drawing code here
+            if (_game._showMenu)
+            {
+                spriteBatch.Begin();
+                spriteBatch.Draw(penguin, new Vector2(50, 100), Color.White);
+                spriteBatch.End();
+                base.Draw(gameTime);
+            }
+
+
+        }
+    }
+}
