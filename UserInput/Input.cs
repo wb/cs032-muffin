0999b06cdea2256cbc34eb8c9a83220a343e3bbd
jonathan.keisuke.yanagawa.kuo <jonathan.keisuke.yanagawa.kuo@100a9738-036a-11de-9992-cbdf800730d7>
2009-03-31 22:59:26 +0000

    input stuff

--- UserInput/Input.cs
+++ UserInput/Input.cs
@@ -1,88 +1,18 @@
 ﻿using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
 
-namespace CS32_Group_Project
+namespace _3D_Renderer
 {
-    public class InputHandler
-    {
-        private KeyboardBindings _keyboardBindings;
-        private GamePadBindings _buttonBindings;
-        private GamePadBindings _thumbstickBindings;
-        private Game _game;
-
-        public InputHandler(Game game)
-        {
-            _game = game;
-            _keyboardBindings = new KeyboardBindings();
-            _buttonBindings = new GamePadBindings();
-            _thumbstickBindings = new GamePadBindings();
-
-            _keyboardBindings.AddInput("quit", Keys.Escape);
-
-            _keyboardBindings.AddInput("left", Keys.Left);
-            _keyboardBindings.AddInput("right", Keys.Right);
-            _keyboardBindings.AddInput("up", Keys.Up);
-            _keyboardBindings.AddInput("down", Keys.Down);
-
-            _buttonBindings.AddInput("left", Buttons.DPadLeft);
-            _buttonBindings.AddInput("right", Buttons.DPadRight);
-            _buttonBindings.AddInput("up", Buttons.DPadUp);
-            _buttonBindings.AddInput("down", Buttons.DPadDown);
-
-            _thumbstickBindings.AddInput("left", Buttons.LeftThumbstickLeft);
-            _thumbstickBindings.AddInput("right", Buttons.LeftThumbstickRight);
-            _thumbstickBindings.AddInput("up", Buttons.LeftThumbstickUp);
-            _thumbstickBindings.AddInput("down", Buttons.LeftThumbstickDown);
-        }
-
-        public bool IsPressed(String inputName)
-        {
-            return _keyboardBindings.IsPressed(inputName) || _buttonBindings.IsPressed(inputName) || _thumbstickBindings.IsPressed(inputName);
-        }
-
-        public bool IsReleased(String inputName)
-        {
-            return _keyboardBindings.IsReleased(inputName) || _buttonBindings.IsReleased(inputName) || _thumbstickBindings.IsReleased(inputName);
-        }
-
-        public bool IsHeld(String inputName)
-        {
-            return _keyboardBindings.IsHeld(inputName) || _buttonBindings.IsHeld(inputName) || _thumbstickBindings.IsHeld(inputName);
-        }
-
-        public bool IsUntouched(String inputName)
-        {
-            return _keyboardBindings.IsUntouched(inputName) && _buttonBindings.IsUntouched(inputName) && _thumbstickBindings.IsUntouched(inputName);
-        }
-
-        public Vector3 UpdatePosition()
-        {
-            _keyboardBindings.UpdateInputState();
-            _buttonBindings.UpdateInputState();
-            _thumbstickBindings.UpdateInputState();
-            if (this.IsPressed("quit"))
-                _game.Exit();
-            Single x = 0, y = 0, z = 0;
-            //if the key is held:
-            if (this.IsHeld("left"))
-                x = -100;
-            if (this.IsHeld("right"))
-                x = 100;
-            if (this.IsHeld("left") && this.IsHeld("right"))
-                x = 0;
-            if (this.IsHeld("up"))
-                y = 100;
-            if (this.IsHeld("down"))
-                y = -100;
-            if (this.IsHeld("up") && this.IsHeld("down"))
-                y = 0;
-            //define actions for IsPressed if needed
-            return new Vector3(x, y, z);
-        }
-    }
-
     public enum InputState
     {
         NULL,
@@ -92,123 +22,16 @@ namespace CS32_Group_Project
         HELD
     }
 
-    public abstract class InputBindings<T>
+    public class InputBinding<T>
     {
-        //abstract methods from which subclasses must extend
-        public abstract void UpdateInputState();
-        public abstract InputState GetState(String inputName);
+        public T _input {get; set;}
+        public int _timeHeld {get; set;}
 
-        protected Dictionary<String, T> _inputHashTable;
-
-        public InputBindings()
+        public InputBinding(T input)
         {
-            _inputHashTable = new Dictionary<String, T>();
+        _input = input;
+        _timeHeld = 0;
         }
 
-        public void AddInput(String inputName, T input)
-        {
-            if (_inputHashTable.ContainsKey(inputName))
-                _inputHashTable.Add(inputName, input);
-            else
-                _inputHashTable.Add(inputName, input);
-        }
-
-        //exceptions? check to see if hashTable contains key?
-        public T GetInput(String inputName)
-        {
-            return _inputHashTable[inputName];
-        }
-
-        public bool IsPressed(String inputName)
-        {
-            return this.GetState(inputName) == InputState.PRESSED;
-        }
-
-        public bool IsReleased(String inputName)
-        {
-            return this.GetState(inputName) == InputState.RELEASED;
-        }
-
-        public bool IsUntouched(String inputName)
-        {
-            return this.GetState(inputName) == InputState.UNTOUCHED;
-        }
-
-        public bool IsHeld(String inputName)
-        {
-            return this.GetState(inputName) == InputState.HELD;
-        }
-    }
-
-    public class KeyboardBindings : InputBindings<Keys>
-    {
-        private KeyboardState oldState;
-        private KeyboardState currentState;
-
-        public KeyboardBindings()
-        {
-            oldState = Keyboard.GetState();
-            currentState = Keyboard.GetState();
-        }
-
-        public override void UpdateInputState()
-        {
-            oldState = currentState;
-            currentState = Keyboard.GetState();
-        }
-
-        public override InputState GetState(String inputName)
-        {
-            InputState keyState = InputState.NULL;
-            if (_inputHashTable.ContainsKey(inputName))
-            {
-                Keys boundKey = this.GetInput(inputName);
-                if (currentState.IsKeyDown(boundKey))
-                    keyState = InputState.PRESSED;
-                else if (oldState.IsKeyDown(boundKey))
-                    keyState = InputState.RELEASED;
-                else
-                    keyState = InputState.UNTOUCHED;
-                if (currentState.IsKeyDown(boundKey) && oldState.IsKeyDown(boundKey))
-                    keyState = InputState.HELD;
-            }
-            return keyState;
-        }
-    }
-
-    public class GamePadBindings : InputBindings<Buttons>
-    {
-        private GamePadState oldState;
-        private GamePadState currentState;
-
-        public GamePadBindings()
-        {
-            oldState = GamePad.GetState(PlayerIndex.One);
-            currentState = GamePad.GetState(PlayerIndex.One);
-        }
-
-        public override void UpdateInputState()
-        {
-            oldState = currentState;
-            currentState = GamePad.GetState(PlayerIndex.One);
-        }
-
-        public override InputState GetState(String inputName)
-        {
-            InputState buttonState = InputState.NULL;
-            if (_inputHashTable.ContainsKey(inputName))
-            {
-                Buttons boundButton = this.GetInput(inputName);
-                if (currentState.IsButtonDown(boundButton))
-                    buttonState = InputState.PRESSED;
-                else if (oldState.IsButtonDown(boundButton))
-                    buttonState = InputState.RELEASED;
-                else
-                    buttonState = InputState.UNTOUCHED;
-                if (currentState.IsButtonDown(boundButton) && oldState.IsButtonDown(boundButton))
-                    buttonState = InputState.HELD;
-            }
-            return buttonState;
-        }
     }
 }
---
+++ UserInput/InputBindings.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace _3D_Renderer
+{
+    public abstract class InputBindings<T>
+    {
+        public int _elapsedTime = 0;
+
+        //abstract methods from which subclasses must extend
+        public abstract void UpdateInputState();
+        public abstract InputState GetState(String inputName);
+
+        protected Dictionary<String, InputBinding<T>> _inputHashTable;
+
+        public void UpdateGameTime(GameTime gameTime)
+        {
+            _elapsedTime = gameTime.ElapsedGameTime.Milliseconds;
+        }
+
+        public InputBindings()
+        {
+            _inputHashTable = new Dictionary<String, InputBinding<T>>();
+        }
+
+        public void AddInput(String inputName, T input)
+        {
+            if (!_inputHashTable.ContainsKey(inputName))
+                _inputHashTable.Add(inputName, new InputBinding<T>(input));
+        }
+
+        //exceptions? check to see if hashTable contains key?
+        public InputBinding<T> GetInput(String inputName)
+        {
+            return _inputHashTable[inputName];
+        }
+
+        public bool IsPressed(String inputName)
+        {
+            return this.GetState(inputName) == InputState.PRESSED;
+        }
+
+        public bool IsReleased(String inputName)
+        {
+            return this.GetState(inputName) == InputState.RELEASED;
+        }
+
+        public bool IsUntouched(String inputName)
+        {
+            return this.GetState(inputName) == InputState.UNTOUCHED;
+        }
+
+        public bool IsHeld(String inputName)
+        {
+            return this.GetState(inputName) == InputState.HELD;
+        }
+    }
+
+    public class KeyboardBindings : InputBindings<Keys>
+    {
+        private KeyboardState oldState;
+        private KeyboardState currentState;
+
+        public KeyboardBindings()
+        {
+            oldState = Keyboard.GetState();
+            currentState = Keyboard.GetState();
+        }
+
+        public override void UpdateInputState()
+        {
+            oldState = currentState;
+            currentState = Keyboard.GetState();
+        }
+
+        public override InputState GetState(String inputName)
+        {
+            InputState keyState = InputState.NULL;
+            if (_inputHashTable.ContainsKey(inputName))
+            {
+                InputBinding<Keys> boundInput = this.GetInput(inputName);
+                Keys boundKey = boundInput._input;
+                if (currentState.IsKeyDown(boundKey))
+                    keyState = InputState.PRESSED;
+                else if (oldState.IsKeyDown(boundKey))
+                    keyState = InputState.RELEASED;
+                else
+                    keyState = InputState.UNTOUCHED;
+                if (currentState.IsKeyDown(boundKey) && oldState.IsKeyDown(boundKey))
+                {
+                    keyState = InputState.HELD;
+                    boundInput._timeHeld += _elapsedTime;
+                }
+                else
+                    boundInput._timeHeld = 0;
+            }
+            return keyState;
+        }
+    }
+
+    public class GamePadBindings : InputBindings<Buttons>
+    {
+        private GamePadState oldState;
+        private GamePadState currentState;
+
+        public GamePadBindings()
+        {
+            oldState = GamePad.GetState(PlayerIndex.One);
+            currentState = GamePad.GetState(PlayerIndex.One);
+        }
+
+        public override void UpdateInputState()
+        {
+            oldState = currentState;
+            currentState = GamePad.GetState(PlayerIndex.One);
+        }
+
+        public override InputState GetState(String inputName)
+        {
+            InputState buttonState = InputState.NULL;
+            if (_inputHashTable.ContainsKey(inputName))
+            {
+                InputBinding<Buttons> boundInput = this.GetInput(inputName);
+                Buttons boundButton = boundInput._input;
+                if (currentState.IsButtonDown(boundButton))
+                    buttonState = InputState.PRESSED;
+                else if (oldState.IsButtonDown(boundButton))
+                    buttonState = InputState.RELEASED;
+                else
+                    buttonState = InputState.UNTOUCHED;
+                if (currentState.IsButtonDown(boundButton) && oldState.IsButtonDown(boundButton))
+                {
+                    buttonState = InputState.HELD;
+                    boundInput._timeHeld += _elapsedTime;
+                }
+                else
+                    boundInput._timeHeld = 0;
+            }
+            return buttonState;
+        }
+    }
+}
